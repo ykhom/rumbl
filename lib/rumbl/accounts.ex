1b146b73a61949f2f2c4ea4ef2cc21b3d561f61b
Yurii Khomitskyi <yurii.khomitskyi@outlook.com>
2020-01-05 17:14:58 +0200

    Added create user & PostgreSQL persistance

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -2,24 +2,32 @@ defmodule Rumbl.Accounts do
   @moduledoc """
     The Accounts context
   """
-
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
-  def list_users do
-    [
-      %User{id: "1", name: "Yurii Khomitskyi", username: "yukhom"},
-      %User{id: "2", name: "Jared Leto", username: "jleto"},
-      %User{id: "3", name: "Kseniia Korchagina", username: "kkorchagina"}
-    ]
+  def get_user(id) do
+    Repo.get(User, id)
   end
 
-  def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def list_users do
+    Repo.all(User)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
   end
 end
